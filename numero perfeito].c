261f573858b936b4f9de11fa6f6d506bfccad6a8
Henrico1234 <henricocostta@gmail.com>
2023-09-23 00:25:47 -0300

    codigo

--- numero perfeito].c
+++ numero perfeito].c
@@ -1,25 +1,27 @@
 #include <stdio.h>
 #include <string.h>
 
-
 void removezeros(char strc[], char dest[])
-
 {
     int tamanhoc = strlen(strc);
     int i = 0;
     int j = 0;
-     if (strc[i] == '-') {
-        dest[j++] = strc[i++]; // Copie o sinal de negativo para o destino
+    if (strc[i] == '-')
+    {
+        dest[j++] = strc[i++];
     }
-    for( i = 1; i < tamanhoc && strc[i] == '0'; i++){
+    for (i = 1; i < tamanhoc && strc[i] == '0'; i++)
+    {
     }
-    for(; i < tamanhoc; i++){
-     dest[j++] = strc[i];
+    for (; i < tamanhoc; i++)
+    {
+        dest[j++] = strc[i];
     }
-    if (j == 1 && dest[0] == '-'){
+    if (j == 1 && dest[0] == '-')
+    {
         dest[j++] = '0';
-}
-dest[j] = '\0';
+    }
+    dest[j] = '\0';
 }
 void soma(char a[], char b[], char c[])
 {
@@ -51,13 +53,7 @@ void soma(char a[], char b[], char c[])
         c[p] = aux;
     }
     c[k] = '\0';
-    removezeros(c, c);
-     if (c[0] == '\0') {
-    strcpy(c, "0");
-     }
-
 }
-
 
 void sub(char a[], char b[], char c[])
 {
@@ -66,24 +62,32 @@ void sub(char a[], char b[], char c[])
 
     int max = (tamanhoa > tamanhob) ? tamanhoa : tamanhob;
 
-    if(tamanhob < tamanhoa){
+    if (tamanhob < tamanhoa)
+    {
 
-        for(int i=tamanhoa,j=tamanhob;i>=0;i--,j--){
-            if(j<0){
-                b[i]='0'; 
+        for (int i = tamanhoa, j = tamanhob; i >= 0; i--, j--)
+        {
+            if (j < 0)
+            {
+                b[i] = '0';
             }
-            else{   
+            else
+            {
                 b[i] = b[j];
             }
         }
     }
-    if(tamanhoa < tamanhob){
+    if (tamanhoa < tamanhob)
+    {
 
-        for(int i=tamanhob,j=tamanhoa;i>=0;i--,j--){
-            if(j<0){
-                a[i]='0';
+        for (int i = tamanhob, j = tamanhoa; i >= 0; i--, j--)
+        {
+            if (j < 0)
+            {
+                a[i] = '0';
             }
-            else{   
+            else
+            {
                 a[i] = a[j];
             }
         }
@@ -93,27 +97,26 @@ void sub(char a[], char b[], char c[])
     int k = 0;
     int borrow = 0;
     int x, y, z;
-     
-    int numericComparison = strcmp(a, b);
-    if (numericComparison < 0)
+
+    int comparacao = strcmp(a, b);
+    if (comparacao < 0)
     {
-        c[k++] = '-'; // Adicione o sinal de negativo
-        // Inverta as strings a e b para garantir que a seja maior em valor absoluto
+        c[k++] = '-';
         char aux[1000];
         strcpy(aux, a);
         strcpy(a, b);
         strcpy(b, aux);
     }
-    else if (numericComparison == 0)
+    else if (comparacao == 0)
     {
-        // Caso especial: se as strings forem numericamente iguais, retorne '0'
+
         c[k++] = '0';
     }
 
     while (i >= 0 || j >= 0 || borrow)
     {
-        x = (i >= 0) ? (a[i] - '0' - i)  : 0;
-        y = (j >= 0) ? (b[j] - '0' - j)  : 0;
+        x = (i >= 0) ? (a[i] - '0' - i) : 0;
+        y = (j >= 0) ? (b[j] - '0' - j) : 0;
         z = x - y - borrow;
         if (z < 0)
         {
@@ -130,44 +133,93 @@ void sub(char a[], char b[], char c[])
         if (j >= 0)
             j--;
     }
-    
 
     int p = k - 1;
     int q = 0;
     for (; q < p; p--, q++)
     {
         int aux = c[q];
-            c[q] = c[p];
-            c[p] = aux;
+        c[q] = c[p];
+        c[p] = aux;
     }
 
-    
-     if (c[k - 1] == '-')
+    if (c[k - 1] == '-')
     {
         for (int p = k - 1; p >= 0; p--)
         {
             c[p + 1] = c[p];
         }
         c[0] = '-';
-       
-}
-       c[k] = '\0';
+    }
+    c[k] = '\0';
     removezeros(c, c);
-    if (c[0] == '\0') {
-    strcpy(c, "0"); // Defina a string como "0"
+    if (c[0] == '\0')
+    {
+        strcpy(c, "0");
+    }
 }
+void mult(char a[], char b[], char c[])
+{
+
+    int tamanhoa = strlen(a);
+    int tamanhob = strlen(b);
+    int tamanhoc = tamanhob + tamanhoa;
+    int resultado[tamanhoc];
+    int carry = 0;
+    int soma;
+
+        if(a == '0'|| b == '0'){
+        c[0] = "0";
+        c[tamanhoc + 1] = '\0';
+    }
+    
+    memset(resultado, 0, sizeof(resultado));
+
+    for (int i = 0; i < tamanhoa; i++)
+    {
+        for (int j = 0; j < tamanhob; j++)
+        {
+            int x = a[tamanhoa - i - 1] - '0';
+            int y = b[tamanhob - j - 1] - '0';
+            resultado[i + j] += x * y;
+        }
+    }
+    for (int i = 0; i < tamanhoc; i++)
+    {
+        soma = resultado[i] + carry;
+        c[i] = (soma % 10) + '0';
+        carry = soma / 10;
+    }
+
+    int p = tamanhoc - 1;
+    int q = 0;
+    for (; q < p; p--, q++)
+    {
+        int aux = c[q];
+        c[q] = c[p];
+        c[p] = aux;
+    }
+    if (c[0] == '0')
+    {
+        removezeros(c, c);
+    }
+    for (int i = 0; tamanhoc > 0 && c[i] == '0'; i++)
+    {
+        tamanhoc--;
+    }
+    c[tamanhoc + 1] = '\0';
 }
 
 int main()
 {
     int n, i;
-    char a[502], b[502], c[1002];
+    char a[502], b[502], c[10000];
     scanf("%d", &n);
     getchar();
     for (i = 0; i < n; i++)
     {
-        scanf("%s",a);
-        scanf("%s",b);
+        scanf("%s", a);
+        scanf("%s", b);
         int o;
         scanf("%d", &o);
         if (o == 1)
@@ -175,12 +227,17 @@ int main()
             soma(a, b, c);
             printf("%s\n", c);
         }
-    
-    if (o == 2)
-    {
-        sub(a, b, c);
-        printf("%s\n", c);
+
+        if (o == 2)
+        {
+            sub(a, b, c);
+            printf("%s\n", c);
+        }
+        if (o == 3)
+        {
+            mult(a, b, c);
+            printf("%s\n", c);
+        }
     }
-}
-return 0;
+    return 0;
 }
